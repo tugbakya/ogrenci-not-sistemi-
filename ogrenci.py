2d13602d428262f00b74f23ce322a59380ac88f7
tugbakya <tugbba.kayaa5523@gmail.com>
2026-05-13 09:54:12 +0300

    Update ogrenci.py

--- ogrenci.py
+++ ogrenci.py
@@ -10,4 +10,28 @@ class Ogrenci:
     def ortalama_hesapla(self):
         if len(self.notlar) == 0:
             return 0
-        return sum(self.notlar) / len(self.notlar)
+
+        toplam = sum(self.notlar)
+        ortalama = toplam / len(self.notlar)
+        return ortalama
+
+    def harf_notu(self):
+        ortalama = self.ortalama_hesapla()
+
+        if ortalama >= 85:
+            return "AA"
+        elif ortalama >= 70:
+            return "BB"
+        elif ortalama >= 50:
+            return "CC"
+        else:
+            return "FF"
+
+    def bilgileri_goster(self):
+        print("---------------------------")
+        print(f"Öğrenci Adı: {self.ad}")
+        print(f"Numara: {self.numara}")
+        print(f"Notlar: {self.notlar}")
+        print(f"Ortalama: {self.ortalama_hesapla():.2f}")
+        print(f"Harf Notu: {self.harf_notu()}")
+        print("---------------------------")
