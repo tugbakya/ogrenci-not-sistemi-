657e7e93fef944f5fd4851d5fdc2b30c09414021
tugbakya <tugbba.kayaa5523@gmail.com>
2026-05-13 09:53:35 +0300

    Update not_islemleri.py

--- not_islemleri.py
+++ not_islemleri.py
@@ -1,5 +1,30 @@
-def ortalama_hesapla(notlar):
-    if len(notlar) == 0:
+def not_kontrol(notu):
+    if 0 <= notu <= 100:
+        return True
+    else:
+        return False
+
+
+def sinif_ortalamasi(ogrenciler):
+    if len(ogrenciler) == 0:
         return 0
 
-    return sum(notlar) / len(notlar)
+    toplam = 0
+
+    for ogrenci in ogrenciler:
+        toplam += ogrenci.ortalama_hesapla()
+
+    return toplam / len(ogrenciler)
+
+
+def en_basarili_ogrenci(ogrenciler):
+    if len(ogrenciler) == 0:
+        return None
+
+    en_iyi = ogrenciler[0]
+
+    for ogrenci in ogrenciler:
+        if ogrenci.ortalama_hesapla() > en_iyi.ortalama_hesapla():
+            en_iyi = ogrenci
+
+    return en_iyi
